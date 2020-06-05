a976873f6b6d083e83cf8b1c43bf68a3fa43c802
Muhammad Hassan <muhammad.hassan@coeus-solutions.de>
2020-06-05 19:34:23 +0500

    Initial commit

---
+++ camera2/src/main/java/us/yydcdut/camera2/MainActivity.java
@@ -0,0 +1,50 @@
+package us.yydcdut.camera2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import us.yydcdut.camera2.ui.PreviewFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getFragmentManager().beginTransaction().replace(R.id.layout_frame_main, PreviewFragment.newInstance()).commit();
+//        getFragmentManager().beginTransaction().replace(R.id.layout_frame_main, new YUVCameraFragment()).commit();
+        getPermissions();
+
+    }
+
+    private void getPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                //Requesting permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
+    @Override //Override from ActivityCompat.OnRequestPermissionsResultCallback Interface
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                }
+                return;
+            }
+        }
+    }
+
+}
